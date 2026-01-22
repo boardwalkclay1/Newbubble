dad4a667bf03ad6574dfda794f931f4ab43c1b5e
boardwalkclay1 <boardwalkclay1@gmail.com>
2026-01-22 14:46:52 -0500

    Create get-location.js

---
+++ js/api/get-location.js
@@ -0,0 +1,22 @@
+import { list } from "@vercel/blob";
+
+export default async function handler(req, res) {
+  const url = new URL(req.url, "http://localhost");
+  const email = url.searchParams.get("email");
+
+  if (!email) {
+    return res.status(400).json({ success: false, error: "Email required" });
+  }
+
+  const safeId = Buffer.from(email).toString("base64url");
+
+  const { blobs } = await list({ prefix: "locations/" });
+  const blob = blobs.find(b => b.pathname.endsWith(`${safeId}.json`));
+
+  if (!blob) {
+    return res.status(200).json({ success: true, location: null });
+  }
+
+  const location = await fetch(blob.url).then(r => r.json());
+  return res.status(200).json({ success: true, location });
+}
